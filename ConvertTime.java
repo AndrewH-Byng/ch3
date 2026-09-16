7e0365144979aaf9c7afeb085bdbc60546e598ac
AndrewH-Byng <2646940@learn.vsb.bc.ca>
2026-09-16 13:32:04 -0700

    Add files via upload

--- ConvertTime.java
+++ ConvertTime.java
@@ -1,1 +1,8 @@
-
+import java.util.Scanner;
+public class ConvertTime {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter numer of seconds");
+		int seconds = in.nextInt();
+		int hours = seconds/360
+		int minutes = 
